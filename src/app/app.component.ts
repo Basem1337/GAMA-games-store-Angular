de5031f4ccb654522202dccc7c03c038cf890ec0
Basem Mohamed <basem.m1337@gmail.com>
2025-03-05 00:13:58 +0200

    add: header and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component} from '@angular/core';
+import { HeaderComponent } from './Components/header/header.component';
+import { FooterComponent } from './Components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [],
+  imports: [HeaderComponent,FooterComponent],
 })
 export class AppComponent {
   
