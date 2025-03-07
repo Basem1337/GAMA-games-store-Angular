4ff64fe9acf9999de9088da6bd11c839f609bb4d
Mohamed <robinvanhood20@gmail.com>
2025-03-07 17:48:23 +0200

    added routers for all pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component} from '@angular/core';
 import { HeaderComponent } from './Components/header/header.component';
 import { FooterComponent } from './Components/footer/footer.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeaderComponent,FooterComponent],
+  imports: [HeaderComponent,FooterComponent,RouterOutlet],
 })
 export class AppComponent {
   
