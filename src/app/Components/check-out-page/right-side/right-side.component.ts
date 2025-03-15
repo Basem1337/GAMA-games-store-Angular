c88775c827523fd675b5f67e91693d1756bf0850
AhmedSobhy <ahmedsobhi.fci@gmail.com>
2025-03-15 11:18:23 +0200

    CheckOut Page Design V01

---
+++ src/app/Components/check-out-page/right-side/right-side.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { GameDetailComponent } from '../game-detail/game-detail.component';
+
+@Component({
+  selector: 'app-right-side',
+  imports: [GameDetailComponent],
+  templateUrl: './right-side.component.html',
+  styleUrl: './right-side.component.css'
+})
+export class RightSideComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component} from '@angular/core';
 import { HeaderComponent } from './Components/header/header.component';
 import { FooterComponent } from './Components/footer/footer.component';
+import { CheckOutPageComponent } from './Components/check-out-page/check-out-page.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeaderComponent,FooterComponent],
+  imports: [HeaderComponent,FooterComponent,CheckOutPageComponent],
 })
 export class AppComponent {
   
