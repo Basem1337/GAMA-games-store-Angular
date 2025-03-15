934077853745c5c584f095934d77895e07ab0c17
yasmin ayad <yasoayad@gmail.com>
2025-03-15 10:34:35 +0200

    about page design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component} from '@angular/core';
 import { HeaderComponent } from './Components/header/header.component';
 import { FooterComponent } from './Components/footer/footer.component';
+import { AboutPageComponent } from "./Components/about-page/about-page.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeaderComponent,FooterComponent],
+  imports: [HeaderComponent, FooterComponent, AboutPageComponent],
 })
 export class AppComponent {
   
