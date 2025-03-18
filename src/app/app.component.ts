d395e27e553130f4185a07749c2156b8d34224c8
AhmedSobhy <ahmedsobhi.fci@gmail.com>
2025-03-18 14:36:39 +0200

    Added Proccessing Page With Responsive Design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component} from '@angular/core';
 import { HeaderComponent } from './Components/header/header.component';
 import { FooterComponent } from './Components/footer/footer.component';
 import { CheckOutPageComponent } from './Components/check-out-page/check-out-page.component';
+import { ProcessingPageComponent } from "./Components/processing-page/processing-page.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeaderComponent,FooterComponent,CheckOutPageComponent],
+  imports: [HeaderComponent, FooterComponent, CheckOutPageComponent, ProcessingPageComponent],
 })
 export class AppComponent {
   
