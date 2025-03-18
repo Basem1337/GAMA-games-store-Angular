2020a36ef11e8479877fbae584e71ccf5d5bf86c
AhmedSobhy <ahmedsobhi.fci@gmail.com>
2025-03-18 15:36:41 +0200

    Added Success Page With Responsive Design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { HeaderComponent } from './Components/header/header.component';
 import { FooterComponent } from './Components/footer/footer.component';
 import { CheckOutPageComponent } from './Components/check-out-page/check-out-page.component';
 import { ProcessingPageComponent } from "./Components/processing-page/processing-page.component";
+import { SuccessPageComponent } from "./Components/success-page/success-page.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeaderComponent, FooterComponent, CheckOutPageComponent, ProcessingPageComponent],
+  imports: [HeaderComponent, FooterComponent, CheckOutPageComponent, ProcessingPageComponent, SuccessPageComponent],
 })
 export class AppComponent {
   
