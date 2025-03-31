45dc40281a0574712e5df2834f91b5b7b5140a95
AhmedSobhy <ahmedsobhi.fci@gmail.com>
2025-03-31 11:51:07 +0200

    Added Error Page With Responsive Design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,13 @@ import { FooterComponent } from './Components/footer/footer.component';
 import { CheckOutPageComponent } from './Components/check-out-page/check-out-page.component';
 import { ProcessingPageComponent } from "./Components/processing-page/processing-page.component";
 import { SuccessPageComponent } from "./Components/success-page/success-page.component";
+import { ErrorPageComponent } from './Components/error-page/error-page.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeaderComponent, FooterComponent, CheckOutPageComponent, ProcessingPageComponent, SuccessPageComponent],
+  imports: [HeaderComponent, FooterComponent, CheckOutPageComponent, ProcessingPageComponent, SuccessPageComponent,ErrorPageComponent],
 })
 export class AppComponent {
   
