6a2d1fcdeb51251247ae7d69713fc1f01cc949b4
alaa alsayed <alaa alsayed@DESKTOP-FA5S6MN>
2025-04-02 03:25:55 +0200

    games page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component} from '@angular/core';
 import { HeaderComponent } from './Components/header/header.component';
 import { FooterComponent } from './Components/footer/footer.component';
+import { ProductsComponent } from './Components/products/products.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeaderComponent,FooterComponent],
+  imports: [HeaderComponent,FooterComponent,ProductsComponent],
 })
 export class AppComponent {
   
